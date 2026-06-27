1b82d8dc9870f4612e0abcbd7e4483045de86e90
Sharad <ksfbd100@gmail.com>
2026-06-27 14:37:28 +0530

    In this commit i wrote code example of a static nested class

--- src/Encapsulation_and_Inheritance/Nested_Class/DemoStactic.java
+++ src/Encapsulation_and_Inheritance/Nested_Class/DemoStactic.java
@@ -3,22 +3,41 @@ package Encapsulation_and_Inheritance.Nested_Class;
 public class DemoStactic {
     public static void main(String[] args) {
         Outer outer = new Outer();
-         Outer.Inner inner = new Outer.Inner();
+         Outer.Inner inner = new Outer.Inner(outer);
          inner.fun();
+         Bankaccount bank = new Bankaccount();
+        System.out.println(bank.computeInterest(1000, 0.09));
 
     }
 }
 
 class Outer{
-//    static int x=0;
-    static void greet(){
-
+    static int x=0;
+    int y = 0;
+    void sayYes(Outer.Inner inner){
+        System.out.println(inner.num);
     }
 
     static class Inner{
+        Outer outer;
+        int num = 2;
+        Inner(Outer outer){
+            this.outer = outer;
+        }
         void fun(){
             System.out.println("Hello");
-            greet(); // Only accessible when greet is static variable
+            System.out.println(outer.y);
         }
+    }
+}
+
+class Bankaccount{
+    static class InterestCalculator{
+        static private double calculateYearly(double principal, double rate){
+            return principal*rate;
+        }
+    }
+    public double computeInterest(double principal, double rate){
+        return InterestCalculator.calculateYearly(principal,rate);
     }
 }
