338a022068e6b3d9b131e228e36b5500e140cc54
Sharad <ksfbd100@gmail.com>
2026-08-17 12:30:33 +0530

    Learned about generics

--- src/Generics/Example2.java
+++ src/Generics/Example2.java
@@ -1,35 +1,35 @@
 package Generics;
 
 public class Example2 {
-    public static void main(String[] args) {
-        Holder<Fish> h1 = new Holder<>();
-    }
-}
+//    public static void main(String[] args) {
+//        Holder<Fish> h1 = new Holder<>();
+//    }
+//}
+//
+//class Holder<T extends Animal & Swimmable>{
+//    T value;
+//}
 
-class Holder<T extends Animal & Swimmable>{
-    T value;
-}
-
- abstract class Animal{
-    abstract void sound();
- }
- interface Swimmable{
-    void swimmable();
- }
- class Dog extends Animal{
-     @Override
-     void sound() {
-         System.out.println("Bark....");
-     }
- }
-
- class Fish  extends Animal implements Swimmable{
-     @Override
-     void sound() {
-         System.out.println("F......");
-     }
-     @Override
-     public void swimmable(){
-         System.out.println("Able to swim");
-     }
+// abstract class Animal{
+//    abstract void sound();
+// }
+// interface Swimmable{
+//    void swimmable();
+// }
+// class Dog extends Animal{
+//     @Override
+//     void sound() {
+//         System.out.println("Bark....");
+//     }
+// }
+//
+// class Fish  extends Animal implements Swimmable{
+//     @Override
+//     void sound() {
+//         System.out.println("F......");
+//     }
+//     @Override
+//     public void swimmable(){
+//         System.out.println("Able to swim");
+//     }
  }
---
+++ src/Generics/Invariance.java
@@ -0,0 +1,52 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invariance {
+    public static void main(String[] args) {
+//        Animal animal = new Dog();
+//        animal.eat();
+//        animal.walk();
+        //animal.bark(); cannot be called
+
+       // List<Dog> dogs = new ArrayList<>();
+       // List<Animal> animals = dogs; This is invariance but safe, as it gives compile time error
+
+
+        // Below is the invariance in array. They aren't allowing animal object to be assigned to animal.
+//        Dog[] dogs = new Dog[10];
+//        Animal[] animals = dogs;
+//
+//        animals[0] = new Dog();
+//        animals[1] = new Dog();
+//        animals[2] = new Dog();
+//        animals[3] = new Dog();
+//        animals[4] = new Dog();
+//        animals[5] = new Animal();
+//
+//
+//        for(Animal an: animals){
+//            if (an==null){
+//                continue;
+//            }
+//            an.eat();
+//        }
+//    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+    void walk(){
+        System.out.println("Walking");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Bark");
+    }
+
+}
